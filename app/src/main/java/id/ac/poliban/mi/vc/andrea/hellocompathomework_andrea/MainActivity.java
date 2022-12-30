aebe6f3bb6a7ec0a3bde01cacf187708905f263e
Andrea Jeane Elquita <andreajeane28.aje@gmail.com>
2022-12-30 12:05:29 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/vc/andrea/hellocompathomework_andrea/MainActivity.java
@@ -0,0 +1,46 @@
+package id.ac.poliban.mi.vc.andrea.hellocompathomework_andrea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelloTextView = findViewById(R.id.hello_textview);
+
+        // restore text color
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+
+        // save text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+
+        // get the colors.xml resource, the integer for the color, and set text color
+        int colorResourceName = getResources().getIdentifier(colorName, "color",
+                getApplicationContext().getPackageName());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        mHelloTextView.setTextColor(colorRes);
+    }
+}
